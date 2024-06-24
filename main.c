fdd0c341d242a9c82e0fd543d57c6b8fceaaafcf
1jss <62594271+1jss@users.noreply.github.com>
2024-06-24 10:55:58 +0200

    Rounded rectangle with se corners

--- main.c
+++ main.c
@@ -32,59 +32,93 @@ f32 calculate_superellipse_radius(f32 radius, f32 t) {
   return radius_pow_2 / powf(cos_pow_4 + sin_pow_4, 0.25f);
 }
 
+void draw_superellipse_border_point(SDL_Renderer *renderer, i32 center_x, i32 center_y, i32 radius, f32 t) {
+  f32 rad = calculate_superellipse_radius(radius, t);
+  f32 float_x = 1.0 * center_x + rad * cos(t);
+  f32 float_y = 1.0 * center_y + rad * sin(t);
+  i32 nearest_x = round(float_x);
+  i32 nearest_y = round(float_y);
+  i32 left_x = (i32)float_x;
+  i32 top_y = (i32)float_y;
+  i32 right_x = left_x + 1;
+  i32 bottom_y = top_y + 1;
+
+  // Smudge rightwards
+  if (t <= M_PI / 4 || t >= 7 * M_PI / 4) {
+    f32 x_diff = float_x - left_x;
+    u8 opacity = (1 - x_diff) * 255;
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, opacity);
+    SDL_RenderDrawPoint(renderer, left_x, nearest_y);
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255 - opacity);
+    SDL_RenderDrawPoint(renderer, right_x, nearest_y);
+  }
+  // Smudge downwards
+  else if (t <= 3 * M_PI / 4) {
+    f32 y_diff = float_y - top_y;
+    u8 opacity = (1 - y_diff) * 255;
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, opacity);
+    SDL_RenderDrawPoint(renderer, nearest_x, top_y);
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255 - opacity);
+    SDL_RenderDrawPoint(renderer, nearest_x, bottom_y);
+  }
+  // Smudge leftwards
+  else if (t <= 5 * M_PI / 4) {
+    f32 x_diff = float_x - right_x;
+    u8 opacity = (1 + x_diff) * 255;
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, opacity);
+    SDL_RenderDrawPoint(renderer, right_x, nearest_y);
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255 - opacity);
+    SDL_RenderDrawPoint(renderer, left_x, nearest_y);
+  }
+  // Smudge upwards
+  else {
+    f32 y_diff = float_y - bottom_y;
+    u8 opacity = (1 + y_diff) * 255;
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, opacity);
+    SDL_RenderDrawPoint(renderer, nearest_x, bottom_y);
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255 - opacity);
+    SDL_RenderDrawPoint(renderer, nearest_x, top_y);
+  }
+}
+
+// Draws a superellipse with a given center and radius
 void draw_superellipse(SDL_Renderer *renderer, i32 center_x, i32 center_y, i32 radius) {
   SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
-
   // Calculate radian step size so that every border pixel is drawn
   f32 step = 0.85 / radius;
-
   for (f32 t = 0; t < 2 * M_PI; t += step) {
-    f32 rad = calculate_superellipse_radius(radius, t);
-    f32 float_x = 1.0 * center_x + rad * cos(t);
-    f32 float_y = 1.0 * center_y + rad * sin(t);
-    i32 nearest_x = round(float_x);
-    i32 nearest_y = round(float_y);
-    i32 left_x = (i32)float_x;
-    i32 top_y = (i32)float_y;
-    i32 right_x = left_x + 1;
-    i32 bottom_y = top_y + 1;
+    draw_superellipse_border_point(renderer, center_x, center_y, radius, t);
+  }
+}
 
-    // Smudge rightwards
-    if (t <= M_PI / 4 || t >= 7 * M_PI / 4) {
-      f32 x_diff = float_x - left_x;
-      u8 opacity = (1 - x_diff) * 255;
-      SDL_SetRenderDrawColor(renderer, 255, 255, 255, opacity);
-      SDL_RenderDrawPoint(renderer, left_x, nearest_y);
-      SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255 - opacity);
-      SDL_RenderDrawPoint(renderer, right_x, nearest_y);
-    }
-    // Smudge downwards
-    else if (t <= 3 * M_PI / 4) {
-      f32 y_diff = float_y - top_y;
-      u8 opacity = (1 - y_diff) * 255;
-      SDL_SetRenderDrawColor(renderer, 255, 255, 255, opacity);
-      SDL_RenderDrawPoint(renderer, nearest_x, top_y);
-      SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255 - opacity);
-      SDL_RenderDrawPoint(renderer, nearest_x, bottom_y);
-    }
-    // Smudge leftwards
-    else if (t <= 5 * M_PI / 4) {
-      f32 x_diff = float_x - right_x;
-      u8 opacity = (1 + x_diff) * 255;
-      SDL_SetRenderDrawColor(renderer, 255, 255, 255, opacity);
-      SDL_RenderDrawPoint(renderer, right_x, nearest_y);
-      SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255 - opacity);
-      SDL_RenderDrawPoint(renderer, left_x, nearest_y);
-    }
-    // Smudge upwards
-    else {
-      f32 y_diff = float_y - bottom_y;
-      u8 opacity = (1 + y_diff) * 255;
-      SDL_SetRenderDrawColor(renderer, 255, 255, 255, opacity);
-      SDL_RenderDrawPoint(renderer, nearest_x, bottom_y);
-      SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255 - opacity);
-      SDL_RenderDrawPoint(renderer, nearest_x, top_y);
-    }
+// Draws a rectangle with superellipse corners
+void draw_rounded_rectangle(SDL_Renderer *renderer, i32 x, i32 y, i32 rectangle_width, i32 rectangle_height, i32 corner_radius) {
+  // Cap corner radius to half of the rectangle width or height
+  if (2 * corner_radius < rectangle_width || 2 * corner_radius < rectangle_height) {
+    corner_radius = rectangle_width < rectangle_height ? rectangle_width / 2 : rectangle_height / 2;
+  }
+  SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
+  // Draw top and bottom lines
+  for (i32 i = x + corner_radius; i <= x + rectangle_width - corner_radius; i++) {
+    SDL_RenderDrawPoint(renderer, i, y);
+    SDL_RenderDrawPoint(renderer, i, y + rectangle_height);
+  }
+  // Draw left and right lines
+  for (i32 i = y + corner_radius; i <= y + rectangle_height - corner_radius; i++) {
+    SDL_RenderDrawPoint(renderer, x, i);
+    SDL_RenderDrawPoint(renderer, x + rectangle_width, i);
+  }
+  // Draw corners
+  f32 step = 0.85 / corner_radius;
+  for (f32 t = 0; t <= M_PI / 2; t += step) {
+    // Bottom right corner (t)
+    draw_superellipse_border_point(renderer, x + rectangle_width - corner_radius, y + rectangle_height - corner_radius, corner_radius, t);
+    // Bottom left corner (t + M_PI / 2)
+    draw_superellipse_border_point(renderer, x + corner_radius, y + rectangle_height - corner_radius, corner_radius, t + M_PI / 2);
+    // Top left corner (t + M_PI)
+    draw_superellipse_border_point(renderer, x + corner_radius, y + corner_radius, corner_radius, t + M_PI);
+    // Top right corner (t + 3 * M_PI / 2)
+    draw_superellipse_border_point(renderer, x + rectangle_width - corner_radius, y + corner_radius, corner_radius, t + 3 * M_PI / 2);
   }
 }
 
@@ -168,11 +202,16 @@ i32 main() {
         return -1;
       }
 
-      // Draw a squircle at mouse position
+      // Draw squircles
       draw_superellipse(renderer, 320, 320, 300);
       draw_superellipse(renderer, 320, 320, 200);
       draw_superellipse(renderer, 320, 320, 100);
       draw_superellipse(renderer, 320, 320, 50);
+
+      // Draw rectangle with rounded corners
+      draw_rounded_rectangle(renderer, 100, 100, 300, 400, 60);
+      draw_rounded_rectangle(renderer, 200, 110, 400, 300, 150);
+      draw_rounded_rectangle(renderer, 10, 10, 600, 40, 30);
 
       // Draw text at mouse position
       SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
