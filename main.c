4db9b4515aae8a946bf3fd0f928aa786b1494a32
1jss <62594271+1jss@users.noreply.github.com>
2024-08-16 12:31:30 +0200

    Handle all events every main loop

--- main.c
+++ main.c
@@ -19,7 +19,6 @@
 i32 main() {
   i32 window_width = 640;
   i32 window_height = 640;
-  i32 scroll_speed = 10;
 
   C9_Gradient white_shade = {
     .start_color = white,
@@ -164,153 +163,156 @@ i32 main() {
   SDL_RaiseWindow(window);
 
   // Begin main loop
-  bool done = false;
-  while (!done) {
+  bool main_loop = true;
+  while (main_loop) {
+    clock_t main_loop_start = clock();
     // Wait for events before rendering
-    if (SDL_WaitEvent(&event)) {
-      if (event.type == SDL_WINDOWEVENT) {
-        if (event.window.event == SDL_WINDOWEVENT_RESIZED) {
-          i32 width = event.window.data1;
-          i32 height = event.window.data2;
-          if (width < min_width) {
-            width = min_width;
+    if (SDL_WaitEvent(NULL)) {
+      // Handle all available events before rendering
+      while (main_loop && SDL_PollEvent(&event)) {
+        if (event.type == SDL_WINDOWEVENT) {
+          if (event.window.event == SDL_WINDOWEVENT_RESIZED) {
+            i32 width = event.window.data1;
+            i32 height = event.window.data2;
+            if (width < min_width) {
+              width = min_width;
+            }
+            if (height < min_height) {
+              height = min_height;
+            }
+            // Cap to min width and height
+            SDL_SetWindowSize(window, width, height);
+            set_dimensions(tree, width, height);
+            // Recreate the target-texture with the new dimensions
+            if (target_texture) {
+              SDL_DestroyTexture(target_texture);
+              target_texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET, width, height);
+              if (!target_texture) {
+                printf("SDL_CreateTexture failed: %s\n", SDL_GetError());
+              }
+            }
+            set_dimensions(tree, width, height);
+            tree->rerender = rerender_type.all;
           }
-          if (height < min_height) {
-            height = min_height;
+        } else if (event.type == SDL_KEYDOWN) {
+          SDL_Keymod mod = SDL_GetModState();
+          // Get key press content
+          SDL_Keysym keysym = event.key.keysym;
+          if (keysym.sym == SDLK_BACKSPACE) {
+            input_handler(tree, "BACKSPACE");
+          } else if (keysym.sym == SDLK_LEFT &&
+                     mod & KMOD_SHIFT) {
+            input_handler(tree, "SELECT_LEFT");
+          } else if (keysym.sym == SDLK_RIGHT &&
+                     mod & KMOD_SHIFT) {
+            input_handler(tree, "SELECT_RIGHT");
+          } else if (keysym.sym == SDLK_DOWN &&
+                     mod & KMOD_SHIFT) {
+            input_handler(tree, "SELECT_END");
+          } else if (keysym.sym == SDLK_UP &&
+                     mod & KMOD_SHIFT) {
+            input_handler(tree, "SELECT_START");
+          } else if (keysym.sym == SDLK_LEFT) {
+            input_handler(tree, "MOVE_LEFT");
+          } else if (keysym.sym == SDLK_RIGHT) {
+            input_handler(tree, "MOVE_RIGHT");
+          } else if (keysym.sym == SDLK_a &&
+                     mod & KMOD_CTRL) {
+            input_handler(tree, "SELECT_ALL");
+          } else if (keysym.sym == SDLK_RETURN) {
+            printf("Return\n");
+          } else if (keysym.sym == SDLK_ESCAPE) {
+            input_handler(tree, "DESELECT");
+          } else if (keysym.sym == SDLK_z &&
+                     mod & KMOD_CTRL) {
+            input_handler(tree, "UNDO");
+          } else if (keysym.sym == SDLK_y &&
+                     mod & KMOD_CTRL) {
+            input_handler(tree, "REDO");
+          } else if (keysym.sym == SDLK_c &&
+                     mod & KMOD_CTRL) {
+            input_handler(tree, "COPY");
+          } else if (keysym.sym == SDLK_x &&
+                     mod & KMOD_CTRL) {
+            input_handler(tree, "CUT");
+          } else if (keysym.sym == SDLK_v &&
+                     mod & KMOD_CTRL) {
+            input_handler(tree, "PASTE");
           }
-          // Cap to min width and height
-          SDL_SetWindowSize(window, width, height);
-          set_dimensions(tree, width, height);
-          // Recreate the target-texture with the new dimensions
-          if (target_texture) {
-            SDL_DestroyTexture(target_texture);
-            target_texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET, width, height);
-            if (!target_texture) {
-              printf("SDL_CreateTexture failed: %s\n", SDL_GetError());
+        } else if (event.type == SDL_TEXTINPUT) {
+          // Get text input content
+          if (!(SDL_GetModState() & KMOD_CTRL)) {
+            char *text = (char *)event.text.text;
+            input_handler(tree, text);
+          }
+        } else if (event.type == SDL_MOUSEWHEEL) {
+          i32 mouse_x = 0;
+          i32 mouse_y = 0;
+          i32 scroll_speed = 10;
+          SDL_GetMouseState(&mouse_x, &mouse_y);
+          i32 scroll_delta_y = event.wheel.y * scroll_speed;
+          i32 scroll_delta_x = event.wheel.x * -scroll_speed;
+
+          // scroll up or down
+          if (event.wheel.y != 0) {
+            i32 remaining_scroll = scroll_y(tree->root, mouse_x, mouse_y, scroll_delta_y);
+            if (remaining_scroll != scroll_delta_y) {
+              set_y(tree->root, 0);
+              tree->rerender = rerender_type.all;
             }
           }
-          set_dimensions(tree, width, height);
-          tree->rerender = rerender_type.all;
-        }
-      } else if (event.type == SDL_KEYDOWN) {
-        SDL_Keymod mod = SDL_GetModState();
-        // Get key press content
-        SDL_Keysym keysym = event.key.keysym;
-        if (keysym.sym == SDLK_BACKSPACE) {
-          input_handler(tree, "BACKSPACE");
-        } else if (keysym.sym == SDLK_LEFT &&
-                   mod & KMOD_SHIFT) {
-          input_handler(tree, "SELECT_LEFT");
-        } else if (keysym.sym == SDLK_RIGHT &&
-                   mod & KMOD_SHIFT) {
-          input_handler(tree, "SELECT_RIGHT");
-        } else if (keysym.sym == SDLK_DOWN &&
-                   mod & KMOD_SHIFT) {
-          input_handler(tree, "SELECT_END");
-        } else if (keysym.sym == SDLK_UP &&
-                   mod & KMOD_SHIFT) {
-          input_handler(tree, "SELECT_START");
-
-        } else if (keysym.sym == SDLK_LEFT) {
-          input_handler(tree, "MOVE_LEFT");
-        } else if (keysym.sym == SDLK_RIGHT) {
-          input_handler(tree, "MOVE_RIGHT");
-        } else if (keysym.sym == SDLK_a &&
-                   mod & KMOD_CTRL) {
-          input_handler(tree, "SELECT_ALL");
-        } else if (keysym.sym == SDLK_RETURN) {
-          printf("Return\n");
-        } else if (keysym.sym == SDLK_ESCAPE) {
-          input_handler(tree, "DESELECT");
-        } else if (keysym.sym == SDLK_z &&
-                   mod & KMOD_CTRL) {
-          input_handler(tree, "UNDO");
-        } else if (keysym.sym == SDLK_y &&
-                   mod & KMOD_CTRL) {
-          input_handler(tree, "REDO");
-        } else if (keysym.sym == SDLK_c &&
-                   mod & KMOD_CTRL) {
-          input_handler(tree, "COPY");
-        } else if (keysym.sym == SDLK_x &&
-                   mod & KMOD_CTRL) {
-          input_handler(tree, "CUT");
-        } else if (keysym.sym == SDLK_v &&
-                   mod & KMOD_CTRL) {
-          input_handler(tree, "PASTE");
-        }
-      } else if (event.type == SDL_TEXTINPUT) {
-        // Get text input content
-        if (!(SDL_GetModState() & KMOD_CTRL)) {
-          char *text = (char *)event.text.text;
-          input_handler(tree, text);
-        }
-      } else if (event.type == SDL_MOUSEWHEEL) {
-        i32 mouse_x = 0;
-        i32 mouse_y = 0;
-        SDL_GetMouseState(&mouse_x, &mouse_y);
-        i32 scroll_delta_y = event.wheel.y * scroll_speed;
-        i32 scroll_delta_x = event.wheel.x * -scroll_speed;
-
-        // scroll up or down
-        if (event.wheel.y != 0) {
-          i32 remaining_scroll = scroll_y(tree->root, mouse_x, mouse_y, scroll_delta_y);
-          if (remaining_scroll != scroll_delta_y) {
-            set_y(tree->root, 0);
-            tree->rerender = rerender_type.all;
+          // scroll left or right
+          if (event.wheel.x != 0) {
+            i32 remaining_scroll = scroll_x(tree->root, mouse_x, mouse_y, scroll_delta_x);
+            if (remaining_scroll != scroll_delta_x) {
+              set_x(tree->root, 0);
+              tree->rerender = rerender_type.all;
+            }
           }
-        }
-        // scroll left or right
-        if (event.wheel.x != 0) {
-          i32 remaining_scroll = scroll_x(tree->root, mouse_x, mouse_y, scroll_delta_x);
-          if (remaining_scroll != scroll_delta_x) {
-            set_x(tree->root, 0);
-            tree->rerender = rerender_type.all;
+          SDL_FlushEvent(SDL_MOUSEWHEEL);
+        } else if (event.type == SDL_MOUSEBUTTONDOWN) {
+          i32 mouse_x = 0;
+          i32 mouse_y = 0;
+          SDL_GetMouseState(&mouse_x, &mouse_y);
+          // Blur former active element
+          blur_handler(tree, 0);
+          // Set new active element
+          tree->active_element = get_clickable_element_at(tree->root, mouse_x, mouse_y);
+          // Set selection if active element has input
+          if (tree->active_element != 0 &&
+              tree->active_element->input != 0) {
+            // Triple click selects all
+            if (event.button.clicks == 3) {
+              input_handler(tree, "SELECT_ALL");
+            } else {
+              i32 relative_x_position = mouse_x - tree->active_element->layout.x - tree->active_element->padding.left - tree->active_element->layout.scroll_x;
+              set_selection(tree->active_element->input, relative_x_position);
+            }
           }
-        }
-        SDL_FlushEvent(SDL_MOUSEWHEEL);
-      } else if (event.type == SDL_MOUSEBUTTONDOWN) {
-        i32 mouse_x = 0;
-        i32 mouse_y = 0;
-        SDL_GetMouseState(&mouse_x, &mouse_y);
-        // Blur former active element
-        blur_handler(tree, 0);
-        // Set new active element
-        tree->active_element = get_clickable_element_at(tree->root, mouse_x, mouse_y);
-        // Set selection if active element has input
-        if (tree->active_element != 0 &&
-            tree->active_element->input != 0) {
-          // Triple click selects all
-          if (event.button.clicks == 3) {
-            input_handler(tree, "SELECT_ALL");
-          } else {
+          click_handler(tree, 0);
+          SDL_FlushEvent(SDL_MOUSEBUTTONDOWN);
+        } else if (event.type == SDL_MOUSEMOTION) {
+          i32 mouse_x = 0;
+          u32 mouse_button_down = SDL_GetMouseState(&mouse_x, NULL);
+          if (mouse_button_down & SDL_BUTTON_LMASK &&
+              tree->active_element != 0 &&
+              tree->active_element->input != 0) {
             i32 relative_x_position = mouse_x - tree->active_element->layout.x - tree->active_element->padding.left - tree->active_element->layout.scroll_x;
-            set_selection(tree->active_element->input, relative_x_position);
+            set_selection_end(tree->active_element->input, relative_x_position);
+            // Set input to rerender
+            tree->active_element->render.changed = 1;
+            // Redraw parent to prevent bleeding corners
+            tree->rerender_element = get_parent(tree, tree->active_element);
+            bump_rerender(tree);
           }
+          SDL_FlushEvent(SDL_MOUSEMOTION);
+        } else if (event.type == SDL_QUIT) {
+          main_loop = false;
         }
-        click_handler(tree, 0);
-        SDL_FlushEvent(SDL_MOUSEBUTTONDOWN);
-      } else if (event.type == SDL_MOUSEMOTION) {
-        i32 mouse_x = 0;
-        u32 mouse_button_down = SDL_GetMouseState(&mouse_x, NULL);
-        if (mouse_button_down & SDL_BUTTON_LMASK &&
-            tree->active_element != 0 &&
-            tree->active_element->input != 0) {
-          i32 relative_x_position = mouse_x - tree->active_element->layout.x - tree->active_element->padding.left - tree->active_element->layout.scroll_x;
-          set_selection_end(tree->active_element->input, relative_x_position);
-          // Set input to rerender
-          tree->active_element->render.changed = 1;
-          // Redraw parent to prevent bleeding corners
-          tree->rerender_element = get_parent(tree, tree->active_element);
-          bump_rerender(tree);
-        }
-        SDL_FlushEvent(SDL_MOUSEMOTION);
-      } else if (event.type == SDL_QUIT) {
-        done = true;
       }
     }
 
     if (tree->rerender == rerender_type.all) {
-      clock_t render_start = clock();
       SDL_SetRenderTarget(renderer, target_texture);
       render_element_tree(renderer, tree);
       // Draw target_texture to back buffer and present
@@ -319,9 +321,6 @@ i32 main() {
       SDL_RenderPresent(renderer);
       tree->rerender = rerender_type.none;
       tree->rerender_element = 0;
-      clock_t render_end = clock();
-      f64 render_time_spent = (f64)(render_end - render_start) / CLOCKS_PER_SEC;
-      printf("Time spent render: %f\n", render_time_spent);
     } else if (tree->rerender == rerender_type.selected && tree->rerender_element != 0) {
       SDL_Rect target_rectangle = {
         .x = tree->rerender_element->layout.x,
@@ -339,7 +338,11 @@ i32 main() {
       tree->rerender_element = 0;
     }
 
-    SDL_Delay(8);
+    clock_t main_loop_end = clock();
+    f64 main_loop_cycle = (f64)(main_loop_end - main_loop_start) / CLOCKS_PER_SEC;
+    if (main_loop_cycle < 0.016) {
+      SDL_Delay((0.016 - main_loop_cycle) * 1000);
+    }
   }; // End of rendering loop
 
   if (target_texture) {
